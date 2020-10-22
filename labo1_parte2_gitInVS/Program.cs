3ac1d263e751359b2b9281e41692c69d421edf2d
utente_globale <utente_globale_email@sayonala.com>
2020-10-22 12:23:01 +0200

    Add project files.

---
+++ labo1_parte2_gitInVS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labo1_parte2_gitInVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
